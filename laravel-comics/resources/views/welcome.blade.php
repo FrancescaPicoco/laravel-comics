14ef54acb81d76d9fc4021fb849b3fa9ca9f6a42
FrancescaPicoco <francesca.picoco33@gmail.com>
2024-01-22 17:32:37 +0100

    .

--- laravel-comics/resources/views/welcome.blade.php
+++ laravel-comics/resources/views/welcome.blade.php
@@ -8,10 +8,9 @@
 <body>
     <h1>ECCOCI</h1>
     <hr>
-   <h2> {{ $messaggio }} </h2>
-   <h2> {{ $nome }} </h2>
-   <a href="/dcComics">vai al sito DC COMICS</a>
- 
+    <h2> {{ $messaggio }} </h2>
+    <h2> {{ $nome }} </h2>
+    <a href="/dcComics">vai al sito DC COMICS</a>
 </body>
 </html>
 
